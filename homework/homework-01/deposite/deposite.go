f6e6b2f62f4f9430f146ec07878fe2b08d0fb472
Alexandr Okhrimenko <asoinf@gmail.com>
2020-04-19 16:53:27 +0400

    files were added

---
+++ homework/homework-01/deposite/deposite.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var percent, deposit float64
+	fmt.Println("Input deposit value")
+	fmt.Scan(&deposit)
+	fmt.Println("Input deposit percent")
+	fmt.Scan(&percent)
+	for i := 1; i < 6; i++ {
+		deposit = deposit + ((deposit * percent) / 100)
+		fmt.Println("after year", i, "deposit gonna be ", fmt.Sprintf("%.2f", deposit))
+	}
+
+}
